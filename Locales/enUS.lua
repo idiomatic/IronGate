35eb58d2fd29f38d5bb557ffb663b2d808d8da04
brian harrison <brie+github@idiomatic.org>
2012-02-14 00:48:29 -0800

    preliminary localization

---
+++ Locales/enUS.lua
@@ -0,0 +1,15 @@
+local L = { }
+IronGate.Locales.enUS = L
+
+L["Welcome to the Ironman Challenge. Watch your step."] = true
+L["Warning"] = true
+L["Disqualified"] = true
+L["(and %d more)"] = true
+L["%s statistic should be 0 but is %s."] = true
+L["%s equipment should be |cff9d9d9dpoor|r or |cffffffffcommon|r."] = true
+L["Player is in a party."] = true
+L["Player is in a raid."] = true
+L["%s buff from %s"] = true
+L["Guild %s is level %d."] = true
+L["Quest \"%s\" may kill you."] = true
+L["Player died."] = true
